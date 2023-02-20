3d16c9a8d95c9b39414dc95d0c66e3ce634daef6
Jean Macedo <jfmacedo91@gmail.com>
2023-02-20 10:42:49 -0300

    add ex004

---
+++ ex004/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Strings</title>
+</head>
+<body>
+  <?php
+    echo "<h1>PHP \u{1F418}</h1>";
+    echo '<h1>PHP \u{1F418}</h1>';
+
+    const ESTADO = "PR";
+    echo '<h2>Moro no '.ESTADO.'\u{1F5FA}</h1>';
+    echo "<h2>Moro no ".ESTADO."\u{1F5FA}</h1>";
+    echo "<h2>Moro no ESTADO \u{1F5FA}</h1>";
+
+    $canal = 'Curso em Vídeo';
+    $ano = date('Y');
+    echo <<< TESTE
+      <pre>Olá gaelra do canal $canal!
+        Tudo bem com vocês?
+
+        Como está sendo o ano de $ano?</pre>
+    TESTE; //Não funciona em HTML, só com a tag <pre>
+  ?>
+</body>
+</html>
